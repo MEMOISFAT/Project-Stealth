0b5545f22e2d0a403e4171f5a64f9c0c7cd92041
NEGALITOR <tahatas123@gmail.com>
2021-06-04 22:08:14 -0400

    Camera Behavior
    LookAt Follow
    
    Bugs
    -rotate around the player as camera moves not implemented

--- Assets/Player/Scripts/CameraBehavior.cs
+++ Assets/Player/Scripts/CameraBehavior.cs
@@ -4,18 +4,48 @@ using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour
 {
+    public Transform cam;
     public Transform player;
     public Vector3 offset;
+    Vector3 direction;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        cam = Camera.main.transform;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = player.transform.position + offset;
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSmoothTime, Vector3.right) * offset;
+        transform.position = player.position + offset;
+    }
+
+
+    void RotateAroundPlayer()
+    {
+
+
+
+        /*float hAxis = Input.GetAxisRaw("Horizontal");
+        float vAxis = Input.GetAxisRaw("Vertical");
+
+        direction = new Vector3(hAxis, 0f, vAxis).normalized;
+
+
+        if (direction.magnitude >= 0.1f)
+        {
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.RotateAround(player.position, Vector3.up, angle);
+                //Quaternion.Euler(0f, angle, 0f);
+
+        }*/
     }
 }
