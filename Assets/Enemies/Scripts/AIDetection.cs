6bc38ea15ae490b85945f2d961f2975ce1a66145
NEGALITOR <tahatas123@gmail.com>
2021-06-18 15:53:26 -0400

    Simple Stealth
    New AI with basic see and follow tactic.
    
    Bugs
    -Camera is jittery for some reason
    -AI needs more work

--- Assets/Enemies/Scripts/AIDetection.cs
+++ Assets/Enemies/Scripts/AIDetection.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIDetection : MonoBehaviour
 {
+    public LayerMask layerMask;
+    private Transform player;
+    private NavMeshAgent agent;
+    public float radius;
+    private bool playerDetected;
+    public float minDist;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerDetected = Physics.CheckSphere(gameObject.transform.position, radius, layerMask, QueryTriggerInteraction.UseGlobal);
+        float dist = Vector3.Distance(transform.position, player.position);
+        Debug.Log(dist);
+
+        if (playerDetected)
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(gameObject.transform.position, radius);
     }
 }
